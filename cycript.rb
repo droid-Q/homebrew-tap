ca8e609a8c7ca87b04ff882baefc20abea30c8e7
顾佳奇 <droidqw@gmail.com>
2018-01-20 10:59:57 +0800

    change

--- cycript.rb
+++ cycript.rb
@@ -1,27 +1,31 @@
-# grossest thing you've ever seen
-
-require "formula"
-
 class Cycript < Formula
-  homepage "http://www.cycript.org/"
-  url "https://cydia.saurik.com/api/latest/3"
-  version "latest"
+  homepage "http://www.cycript.org"
+  url "https://cache.saurik.com/cycript/mac/cycript_0.9.594.zip"
+  sha256 "6e0ded92e03ea44651a26a23158f82d8c939020960fbb3b47102a6b73770d381"
+
+  patch :DATA
 
   def install
-    script = "cd /usr/local/Cellar/cycript/latest/bin && ./_cycript $*"
-    bin.install Dir["./*"]
-    File.rename("/usr/local/Cellar/cycript/latest/bin/cycript",
-    "/usr/local/Cellar/cycript/latest/bin/_cycript")
-    File.open("/usr/local/Cellar/cycript/latest/bin/cycript", "w") do |f|
-      f.write(script)
-    end
+    bin.install "cycript"
+    lib.install Dir["Cycript.*"]
   end
 
-  test do
-    system "true"
-  end
-
-  def caveats
-    system "chmod", "+x", "/usr/local/Cellar/cycript/latest/bin/cycript"
+  def post_install
+    chmod 0755, bin/"cycript"
   end
 end
+
+__END__
+diff --git i/cycript w/cycript
+index 077458e..7874f02 100644
+--- i/cycript
++++ w/cycript
+@@ -17,7 +17,7 @@
+ # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ # }}}
+
+-dir=${0%/*}/Cycript.lib
++dir=($(brew --prefix cycript)/lib/Cycript.lib)
+ if [[ ${dir} != /* ]]; then
+     dir=$(pwd)/${dir}
+ fi
