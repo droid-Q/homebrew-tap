68a982c7896463350447dd33a576209af7c60cc6
Jiaqi Gu <droidqw@gmail.com>
2019-10-13 10:03:41 +0800

    add service

--- aria2.rb
+++ aria2.rb
@@ -3,17 +3,12 @@ class Aria2 < Formula
   homepage "https://aria2.github.io/"
   url "https://github.com/aria2/aria2/releases/download/release-1.35.0/aria2-1.35.0.tar.xz"
   sha256 "1e2b7fd08d6af228856e51c07173cfcf987528f1ac97e04c5af4a47642617dfd"
+
   patch do
-	  url "https://raw.githubusercontent.com/droid-Q/aria2-patch/master/myaria2.patch"
-	  sha256 "ac30ddd38383775ce69f0558b24ba9b12db4c8ef2f9e9a9f22c6d6d8bdd78b6c"
+	url "https://raw.githubusercontent.com/droid-Q/aria2-patch/master/myaria2.patch"
+	sha256 "ac30ddd38383775ce69f0558b24ba9b12db4c8ef2f9e9a9f22c6d6d8bdd78b6c"
   end
-  bottle do
-    cellar :any
-    sha256 "9cc5e04be8b0a58d1f2b60b8abfc636168edbf23e7018003c40f1dd6952aab0c" => :catalina
-    sha256 "761836ac608eb0a59d4a6f6065860c0e809ce454692e0937d9d0d89ad47f3ce4" => :mojave
-    sha256 "70cc7566a23c283015368f92dfeaa0d119e53cfc7c1b2276a73ff9f6167b529d" => :high_sierra
-  end
-  
+
   depends_on "pkg-config" => :build
   depends_on "libssh2"
 
@@ -37,7 +32,25 @@ class Aria2 < Formula
 
     bash_completion.install "doc/bash_completion/aria2c"
   end
-
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/httpd</string>
+        <string>-D</string>
+        <string>FOREGROUND</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+    </dict>
+    </plist>
+  EOS
+  end
   test do
     system "#{bin}/aria2c", "https://brew.sh/"
     assert_predicate testpath/"index.html", :exist?, "Failed to create index.html!"
