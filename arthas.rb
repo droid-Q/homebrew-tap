84cbbde5a05e899e4e4a4fdf04ef7a373784275e
Jiaqi Gu <droidqw@gmail.com>
2019-11-27 12:46:29 +0800

    upgrade arthas

--- arthas.rb
+++ arthas.rb
@@ -1,7 +1,7 @@
 class Arthas < Formula
   desc "Alibaba Java Diagnostic Tool Arthas/Alibaba Java诊断利器Arthas"
   homepage "https://alibaba.github.io/arthas/"
-  version "3.1.4"
+  version "3.1.5"
   url "https://github.com/alibaba/arthas/releases/download/arthas-all-#{version}/arthas-#{version}-bin.zip"
   sha256 "5adf41f2b4577d09981ae32d79596dd237129f033a8240179997efb2f7514820"
 
