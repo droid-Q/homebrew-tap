8c6a832d1d498b0f83ebd7f06a996df61a58843d
Jiaqi Gu <droidqw@gmail.com>
2019-12-10 23:23:15 +0800

    upgrade

--- baidupcs-go.rb
+++ baidupcs-go.rb
@@ -1,7 +1,7 @@
 class BaidupcsGo < Formula
     desc "仿 Linux shell 文件处理命令的百度网盘命令行客户端"
     homepage "https://github.com/iikira/BaiduPCS-Go"
-    version "3.6"
+    version "3.6.1"
     url "https://github.com/iikira/BaiduPCS-Go/releases/download/v#{version}/BaiduPCS-Go-v#{version}-darwin-osx-amd64.zip"
     sha256 "2335895027b290a0ff9fd537652e4ae6427acc03450b6fda80f947e40ea042a8"
 
