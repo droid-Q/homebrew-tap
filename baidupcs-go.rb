5d2497e024355bd00f06921c5813770d6ac6f88b
Jiaqi Gu <droidqw@gmail.com>
2020-04-26 17:13:12 +0800

    Update baidupcs-go.rb

--- baidupcs-go.rb
+++ baidupcs-go.rb
@@ -1,9 +1,9 @@
 class BaidupcsGo < Formula
     desc "仿 Linux shell 文件处理命令的百度网盘命令行客户端"
     homepage "https://github.com/iikira/BaiduPCS-Go"
-    version "3.6.1"
+    version "3.6.2"
     url "https://github.com/iikira/BaiduPCS-Go/releases/download/v#{version}/BaiduPCS-Go-v#{version}-darwin-osx-amd64.zip"
-    sha256 "bb5989cdeaeea721932eee97fbadc721c8c1500f0916157c619eac80f235ae35"
+    sha256 "547ab39ab4f205c4323337a8e0c250ab6f51502a3812fb6bd5a32bf2b25b0f14"
 
     def install
         bin.install "BaiduPCS-Go"
