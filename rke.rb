cf4d6d15b35e9bdaf28d3a909fda337a0f41df3d
Jiaqi Gu <droidqw@gmail.com>
2020-05-14 08:16:53 +0800

    Update rke.rb

--- rke.rb
+++ rke.rb
@@ -2,9 +2,9 @@ class Rke < Formula
   desc "Rancher Kubernetes Engine, a Kubernetes installer that works everywhere"
   homepage "https://rancher.com/docs/rke/v0.1.x/en/"
   head "https://github.com/rancher/rke.git"
-  url "https://github.com/rancher/rke.git",
-      :tag      => "v1.1.1",
-      :revision => "65f946ae5c798577b282e8b3fd12974b41f43bf7"
+  # url "https://github.com/rancher/rke.git",
+  #     :tag      => "v1.1.1",
+  #     :revision => "65f946ae5c798577b282e8b3fd12974b41f43bf7"
 
   bottle do
     cellar :any_skip_relocation
