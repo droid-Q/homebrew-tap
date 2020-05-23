47ea25b6dd8d39f36859107ee695e505b2fc7b43
Jiaqi Gu <droidqw@gmail.com>
2020-05-23 16:31:27 +0800

    Update arthas.rb

--- arthas.rb
+++ arthas.rb
@@ -5,6 +5,8 @@ class Arthas < Formula
   url "https://github.com/alibaba/arthas/releases/download/arthas-all-#{version}/arthas-#{version}-bin.zip"
   sha256 "83b3957bf8bf680e547fbd17a19ed66c5ab2c9e7a539e50ff7d4fcd597ea3c75"
 
+  depends_on "telnet"
+  
   def install
     bin.install "as.sh" => "as"
   end
