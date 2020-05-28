b45ad89868e0f3f977c213b32ebbfaab51c721e7
Jiaqi Gu <droidqw@gmail.com>
2020-05-28 15:26:13 +0800

    Update istioctl.rb

--- istioctl.rb
+++ istioctl.rb
@@ -3,7 +3,7 @@ class Istioctl < Formula
   homepage "https://github.com/istio/istio"
   version "1.6.0"
   url "https://github.com/istio/istio/releases/download/#{version}/istioctl-#{version}-osx.tar.gz"
-  sha256 "83b3957bf8bf680e547fbd17a19ed66c5ab2c9e7a539e50ff7d4fcd597ea3c75"
+  sha256 "83bf64e1d9c8ae12610e3f8d19a6f60067f3d826058c363d5cfdaadaae31f262"
   depends_on "docker" => :build
   depends_on "go" => :build
   def install
