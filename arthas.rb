2bb91c90bddaebb11fe1e6b42c4e583ca80e53cc
Jiaqi Gu <droidqw@gmail.com>
2021-01-18 16:20:32 +0800

    Update arthas.rb

--- arthas.rb
+++ arthas.rb
@@ -1,9 +1,9 @@
 class Arthas < Formula
   desc "Alibaba Java Diagnostic Tool Arthas/Alibaba Java诊断利器Arthas"
   homepage "https://alibaba.github.io/arthas/"
-  version "3.2.0"
-  url "https://github.com/alibaba/arthas/releases/download/arthas-all-#{version}/arthas-#{version}-bin.zip"
-  sha256 "83b3957bf8bf680e547fbd17a19ed66c5ab2c9e7a539e50ff7d4fcd597ea3c75"
+  version "3.4.6"
+  url "https://github.com/alibaba/arthas/releases/download/arthas-all-#{version}/arthas-bin.zip"
+  sha256 "3d05b6f992177781a5d9cf8593c9b418a3f2803ee337611ecae47274703accad"
 
   depends_on "telnet"
   
