b6636a2f0d77d6e75814e2f2d35d7c16821fe6a7
Jiaqi Gu <droidqw@gmail.com>
2021-04-04 13:24:49 +0800

    Update rke.rb

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -6,9 +6,10 @@ class Rke < Formula
   license "Apache-2.0"
 
   bottle do
-    root_url "https://homebrew.bintray.com/bottles-tap"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, big_sur: "11584391bd69efcb4f1eb21c5060b4b83124a491b3719bb4e6785de63a189068"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "98bf45da784fd5d5a04c260da59d9caaff0ec68834e71c0145d77098f9e37c87"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a68b93b323ce7be29ba12ab22c322da0885cc063a826d4ba0ad247adc2a7cffb"
+    sha256 cellar: :any_skip_relocation, catalina:      "c0842837c7c28f08a3376ddabfcf0c4b6bb6cb6454fdba8a55039272aa096526"
+    sha256 cellar: :any_skip_relocation, mojave:        "e1c7834dcf839f47022708081678009cd10c054db8e1802c0d32fb784a5a7e1b"
   end
 
   depends_on "go" => :build
