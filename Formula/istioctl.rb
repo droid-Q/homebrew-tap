9641110eb4691974b92a4a15f55e941d6d1042a8
Jiaqi Gu <droidqw@gmail.com>
2021-04-04 17:37:47 +0800

    fix

--- Formula/istioctl.rb
+++ Formula/istioctl.rb
@@ -1,8 +1,8 @@
 class Istioctl < Formula
   desc "Istio configuration command-line utility"
   homepage "https://github.com/istio/istio"
-  url "https://github.com/istio/istio/releases/download/#{version}/istioctl-#{version}-osx.tar.gz"
   version "1.6.2"
+  url "https://github.com/istio/istio/releases/download/#{version}/istioctl-#{version}-osx.tar.gz"
   sha256 "242c1999050c2df41e5e85525a10354247acf6562bace481c5d0eda11e66ac83"
   depends_on "docker" => :build
   depends_on "go" => :build
