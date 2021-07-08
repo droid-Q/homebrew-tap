49b7420c9852d707ded17d20926d8a4c0b6a376d
Jiaqi Gu <droidqw@gmail.com>
2021-07-08 18:50:27 +0800

    Update Formulas

--- Formula/arthas.rb
+++ Formula/arthas.rb
@@ -1,6 +1,6 @@
 class Arthas < Formula
   desc "Alibaba Java Diagnostic Tool Arthas/Alibaba Java诊断利器Arthas"
-  homepage "https://alibaba.github.io/arthas/" 
+  homepage "https://alibaba.github.io/arthas/"
   url "https://github.com/alibaba/arthas/releases/download/arthas-all-3.5.2/arthas-bin.zip"
   sha256 "2f87ad60b09d30b85e96d85a46ffb32a1e80af483ebf8da4e86d469285386a5b"
 
