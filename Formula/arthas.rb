19af1e9417a83aa445b906f8f408c9ced49e9205
Jiaqi Gu <droidqw@gmail.com>
2021-07-08 10:42:43 +0000

    arthas: add 3.5.2 bottle.

--- Formula/arthas.rb
+++ Formula/arthas.rb
@@ -4,6 +4,12 @@ class Arthas < Formula
   url "https://github.com/alibaba/arthas/releases/download/arthas-all-3.5.2/arthas-bin.zip"
   sha256 "2f87ad60b09d30b85e96d85a46ffb32a1e80af483ebf8da4e86d469285386a5b"
 
+  bottle do
+    root_url "https://github.com/droid-Q/homebrew-tap/releases/download/arthas-3.5.2"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, catalina: "63f1f9acee56fb80fd059969c5df5a7d2f3740f8aed753f295955d4cf0903a3f"
+  end
+
   depends_on "telnet"
 
   def install
