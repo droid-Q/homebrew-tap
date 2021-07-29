874b8c3e390774eb3aa48c8e30f87984da299b92
Jiaqi Gu <droidqw@gmail.com>
2021-07-29 01:23:23 +0000

    arthas: add 3.5.3 bottle.

--- Formula/arthas.rb
+++ Formula/arthas.rb
@@ -7,6 +7,12 @@ class Arthas < Formula
   license "Apache-2.0"
   head "https://github.com/alibaba/arthas"
 
+  bottle do
+    root_url "https://github.com/droid-Q/homebrew-tap/releases/download/arthas-3.5.3"
+    sha256 cellar: :any_skip_relocation, big_sur:      "527dd7f57f1c51793bc76a7f8b5d9f1ad303d4d2dbefd4b0bb29bfce7590f450"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "6f5d2fa24ed7a4d8e10a89f75a8b8ae8434842a46a9246d14bef15ae8bdd37e6"
+  end
+
   depends_on "openjdk@8" => :build
   depends_on "maven" => :build
 
