b0d3d0a710d6913deaa3ff344ddc7a28d728e40c
Jiaqi Gu <droidqw@gmail.com>
2021-11-22 16:31:25 +0800

    Update istioctl.rb

--- Formula/istioctl.rb
+++ Formula/istioctl.rb
@@ -16,6 +16,13 @@ class Istioctl < Formula
   depends_on "go" => :build
   depends_on "go-bindata" => :build
 
+  # Fix https://github.com/istio/istio/issues/35831
+  # remove in next release
+  patch do
+    url "https://github.com/istio/istio/commit/6d9c69f10431bca2ee2beefcfdeaad5e5f62071b.patch?full_index=1"
+    sha256 "47e175fc0ac5e34496c6c0858eefbc31e45073dad9683164f7a21c74dbaa6055"
+  end
+
   def install
     # make parallelization should be fixed in version > 1.11.0
     ENV.deparallelize
