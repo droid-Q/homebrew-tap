ba24f48b142634de0caf6f81e160bad39b2e921b
Jiaqi Gu <droidqw@gmail.com>
2021-12-20 05:34:30 +0000

    rke: update 1.3.3 bottle.

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -6,9 +6,9 @@ class Rke < Formula
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/droid-Q/homebrew-tap/releases/download/rke-1.3.2"
-    sha256 cellar: :any_skip_relocation, big_sur:      "4be055e6a9daa99c32f7cc70a897800181e2c529810cccf353d9df6139ccc2af"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "d7aef1d12f5e737c45f08f74b5263dab972daef7412e1ad1540d8329a88e2052"
+    root_url "https://github.com/droid-Q/homebrew-tap/releases/download/rke-1.3.3"
+    sha256 cellar: :any_skip_relocation, big_sur:      "15468ba9e1cb4b7a4a489b3bb516370b2a7838eca1e4729c171c20c7b822ea39"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8227bd3fe121cf28fe5af78fa94860f71c544fde8c85fa2fa8d7f0ef45548684"
   end
 
   depends_on "go" => :build
